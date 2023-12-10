4613819df1eb2f3c52754ae629ff104ee448ddf8
samarpro <samarpangupta123@gmail.com>
2023-12-10 23:51:24 +0545

    Intial Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from './Message'
+
+
+function App(){
+  return <Message />
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// React uses function-based components
+
+function Message(){
+    return <h1>Hello World</h1>
+}
+
+
+export default Message
